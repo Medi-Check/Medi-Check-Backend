9e684131d2a9ac689f7a1d8a297efd95b6c7826b
SewoongPark <lympsw12@naver.com>
2023-10-25 01:14:16 +0900

    service 세팅

--- src/main/java/com/example/medicheckbackend/service/TakeMedicineDayService.java
+++ src/main/java/com/example/medicheckbackend/service/TakeMedicineDayService.java
@@ -1,4 +1,9 @@
 package com.example.medicheckbackend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class TakeMedicineDayService {
 }
