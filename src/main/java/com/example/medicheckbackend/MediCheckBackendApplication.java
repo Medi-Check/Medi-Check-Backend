cf7a4d3c31e946487172c83ba961574afde023c2
SewoongPark <lympsw12@naver.com>
2023-10-24 14:47:20 +0900

    프로젝트 추가

---
+++ src/main/java/com/example/medicheckbackend/MediCheckBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicheckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediCheckBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediCheckBackendApplication.class, args);
+    }
+
+}
