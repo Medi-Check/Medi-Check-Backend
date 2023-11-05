f7fb9a691f05722577300b9308436e013c503a6b
SewoongPark <lympsw12@naver.com>
2023-11-05 16:15:29 +0900

    build.grade 추가

--- src/main/java/com/example/medicheckbackend/repository/MemberRepository.java
+++ src/main/java/com/example/medicheckbackend/repository/MemberRepository.java
@@ -4,7 +4,8 @@ import com.example.medicheckbackend.domain.member.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface
+MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByNickName(String name);
 
     List<Member> findAllByFamilyCode(String familyCode);
